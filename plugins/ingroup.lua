13870d5d017e175a813cc7b741375c4fcdf071b3
kiarash14 <omidbakhsh22@gmail.com>
2016-01-22 16:26:05 +0330

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -527,7 +527,7 @@ local function set_group_photo(msg, success, result)
   end
 end
 
-local function p(receiver, member_username, member_id)
+local function promote(receiver, member_username, member_id)
   local data = load_data(_config.moderation.data)
   local group = string.gsub(receiver, 'chat#id', '')
   if not data[group] then
@@ -541,7 +541,7 @@ local function p(receiver, member_username, member_id)
   return send_large_msg(receiver, member_username..' has been promoted.')
 end
 
-local function p_by_reply(extra, success, result)
+local function promote_by_reply(extra, success, result)
     local msg = result
     local full_name = (msg.from.first_name or '')..' '..(msg.from.last_name or '')
     if msg.from.username then
@@ -555,7 +555,7 @@ local function p_by_reply(extra, success, result)
     end  
 end
 
-local function d(receiver, member_username, member_id)
+local function demote(receiver, member_username, member_id)
   local data = load_data(_config.moderation.data)
   local group = string.gsub(receiver, 'chat#id', '')
   if not data[group] then
@@ -569,7 +569,7 @@ local function d(receiver, member_username, member_id)
   return send_large_msg(receiver, member_username..' has been demoted.')
 end
 
-local function d_by_reply(extra, success, result)
+local function demote_by_reply(extra, success, result)
     local msg = result
     local full_name = (msg.from.first_name or '')..' '..(msg.from.last_name or '')
     if msg.from.username then
@@ -579,7 +579,7 @@ local function d_by_reply(extra, success, result)
     end
     local member_id = msg.from.id
     if msg.to.type == 'chat' then
-      return d(get_receiver(msg), member_username, member_id)
+      return demote(get_receiver(msg), member_username, member_id)
     end  
 end
 
@@ -595,7 +595,7 @@ local function setowner_by_reply(extra, success, result)
       return send_large_msg(receiver, text)
 end
 
-local function p_d_res(extra, success, result)
+local function promote_demote_res(extra, success, result)
 --vardump(result)
 --vardump(extra)
       local member_id = result.id
@@ -603,9 +603,9 @@ local function p_d_res(extra, success, result)
       local chat_id = extra.chat_id
       local mod_cmd = extra.mod_cmd
       local receiver = "chat#id"..chat_id
-      if mod_cmd == 'p' then
+      if mod_cmd == 'promote' then
         return promote(receiver, member_username, member_id)
-      elseif mod_cmd == 'd' then
+      elseif mod_cmd == 'demote' then
         return demote(receiver, member_username, member_id)
       end
 end
@@ -889,7 +889,7 @@ local function run(msg, matches)
       save_data(_config.moderation.data, data)
       return 'Please send me new group photo now'
     end
-    if matches[1] == 'p' and not matches[2] then
+    if matches[1] == 'promote' and not matches[2] then
       if not is_owner(msg) then
         return "Only the owner can prmote new moderators"
       end
@@ -897,7 +897,7 @@ local function run(msg, matches)
           msgr = get_message(msg.reply_id, promote_by_reply, false)
       end
     end
-    if matches[1] == 'p' and matches[2] then
+    if matches[1] == 'promote' and matches[2] then
       if not is_momod(msg) then
         return
       end
@@ -908,14 +908,14 @@ local function run(msg, matches)
         savelog(msg.to.id, name_log.." ["..msg.from.id.."] promoted @".. member)
 	local cbres_extra = {
 	chat_id = msg.to.id,
-        mod_cmd = 'p', 
+        mod_cmd = 'promote', 
 	from_id = msg.from.id
 	}
 	local username = matches[2]
 	local username = string.gsub(matches[2], '@', '')
 	return res_user(username, promote_demote_res, cbres_extra)
     end
-    if matches[1] == 'd' and not matches[2] then
+    if matches[1] == 'demote' and not matches[2] then
       if not is_owner(msg) then
         return "Only the owner can demote moderators"
       end
@@ -923,7 +923,7 @@ local function run(msg, matches)
           msgr = get_message(msg.reply_id, demote_by_reply, false)
       end
     end
-    if matches[1] == 'd' and matches[2] then
+    if matches[1] == 'demote' and matches[2] then
       if not is_momod(msg) then
         return
       end
@@ -937,7 +937,7 @@ local function run(msg, matches)
         savelog(msg.to.id, name_log.." ["..msg.from.id.."] demoted @".. member)
 	local cbres_extra = {
 	chat_id = msg.to.id,
-        mod_cmd = 'd', 
+        mod_cmd = 'demote', 
 	from_id = msg.from.id
 	}
 	local username = matches[2]
@@ -1228,14 +1228,14 @@ return {
   "^[!/](about)$",
   "^[!/](setname) (.*)$",
   "^[!/](setphoto)$",
-  "^[!/](p) (.*)$",
-  "^[!/](p)",
+  "^[!/](promote) (.*)$",
+  "^[!/](promote)",
   "^[!/](help)$",
   "^[!/](clean) (.*)$",
   "^[!/](kill) (chat)$",
   "^[!/](kill) (realm)$",
-  "^[!/](d) (.*)$",
-  "^[!/](d)",
+  "^[!/](demote) (.*)$",
+  "^[!/](demote)",
   "^[!/](set) ([^%s]+) (.*)$",
   "^[!/](lock) (.*)$",
   "^[!/](setowner) (%d+)$",
